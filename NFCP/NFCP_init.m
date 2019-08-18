7b9912f910272a57627101c7340121eeca227934
michaelerule <mrule7404@gmail.com>
2019-08-18 17:10:43 +0100

    removed unused files from examples 1 and 2

--- NFCP/NFCP_init.m
+++ NFCP/NFCP_init.m
@@ -7,8 +7,11 @@ addpath([basepath '/matrix']);
 addpath([basepath '/output']);
 addpath([basepath '/optimize']);
 addpath([basepath '/spikes']);
-addpath([basepath '/EP']);
 addpath([basepath '/measurement']);
-addpath([basepath '/optimize/gpucb_gpml']);
+
+% These were removed 20190 08 18
+% addpath([basepath '/EP']);
+% addpath([basepath '/optimize/gpucb_gpml']);
+
 functional_macros;
 rng('shuffle');  
